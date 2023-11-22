21d63662bc7a2f5d5bb044aa7aa7b4be56e0cc96
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-22 08:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+let list = document.getElementById('infi-list');
+let i = 1; 
+for(; i <= 10; i++){
+	let li = document.createElement('li');
+	li.innerText = `item ${i}`;
+	list.append(li);
+}
+function addTwoElement(){
+	let li = document.createElement('li');
+	li.innerText = `item ${i++}`;
+	let li1 = document.createElement('li');
+	li1.innerText = `item ${i++}`;
+	list.append(li,li1);
+}
+list.addEventListener('scroll', () => {
+	let diff = list.scrollHeight - list.clientHeight;
+	let scrolled = list.scrollTop;
+	if(diff - scrolled <=7){
+		addTwoElement();
+	}
+})
